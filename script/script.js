b8bb22ca003e92f17eb13133493f198ce3270c34
ussdi <usenovadilnaz00@gmail.com>
2025-12-16 13:32:52 +0500

    Add script folder with files

---
+++ script/script.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+console.log("This is a sample JavaScript file.");
+console.log("This is Ussenova Dilnaz");
+// Переходим при нажатии на кнопку ОК
+// это однострочный комментарий
+alert("Click OK to continue on the site.");
+/*
+Это многострочный комментарий
+следующая строка кода выводит сообщение на экран
+*/
+console.log("You have clicked OK and continued on the site.");
